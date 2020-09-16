50acb4b1ce4314a88de5abba2e95614d72163692
supercode7000 <anass@super-code.de>
2020-09-16 16:51:07 +0200

    fix

--- component/Footer.js
+++ component/Footer.js
@@ -3,16 +3,23 @@ import Link from 'next/link'
 const Footer = (props) => {
     return (
         <footer id="footer">
-            <div className="links">
-                <Link href="/impressum"><a>Impressum</a></Link>
-                <Link href="/dsgvo"><a>DSGVO</a></Link>
-            </div>
+
             <div className="copyright">
-                <p className="white"><img src="/superdigital_logo.svg" alt="" /></p>
+
+                <div className="flex">
+                    <p className="white"><img src="/superdigital_logo.svg" alt="" /></p>
+                    <Link href="https://www.facebook.com/superdigitalev"><a><img src="/iconmonstr-facebook-5.svg" alt="" /></a></Link>
+                    <Link href="https://www.instagram.com/superdigitalev/"><a><img src="/iconmonstr-instagram-11.svg" alt="" /></a></Link>
+                    <Link href="https://www.linkedin.com/company/superdigitalev"><a><img src="/iconmonstr-linkedin-5.svg" alt="" /></a></Link>
+                </div>
+                <div className="links">
+                    <Link href="/impressum"><a>Impressum</a></Link>
+                    <Link href="/dsgvo"><a>DSGVO</a></Link>
+                </div>
                 <p>&copy; 2020</p>
             </div>
             <style jsx>{`
-            img{
+            .white img{
                 width:150px
             }
             #footer {
@@ -37,6 +44,13 @@ const Footer = (props) => {
                 font-weight: 200;
                 padding-top: .8em;
             }
+            .flex{
+                display:flex
+            }
+            .flex img{
+                height:30px;
+                padding-right:10px
+            }
             .links {
                 display: flex;
                 justify-content: space-around;
@@ -49,7 +63,7 @@ const Footer = (props) => {
             a {
                 text-decoration: none;
                 display: block;
-                padding: .5em 0;
+                padding: .5em 5px;
                 color: #fff;
             }
             a:visited {
@@ -84,7 +98,8 @@ const Footer = (props) => {
             .white {
                 color: #fff;
                 font-weight: bold;
-                font-size: 1.2em
+                font-size: 1.2em;
+                margin-top:4px;
             }
             @media (max-width: 768px) {
                 .footer-grid {
