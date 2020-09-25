0a254e6d35b5bb192700873397a6199e2265326b
supercode7000 <anass@super-code.de>
2020-09-25 17:59:40 +0200

    e

--- component/Layout.js
+++ component/Layout.js
@@ -10,11 +10,12 @@ class Layout extends Component {
     return (
       <div id="super-layout">
         <Head>
-          <meta property="og:image:secure" content="https://www.super-code.de/img/page-preview.jpg" />
-          <meta property="og:image" content="http://www.super-code.de/img/page-preview.jpg" />
-          <meta property="og:url" content="https://www.super-code.de" />
+          <meta property="og:image:secure" content="https://www.super-stipendium.de/img/page-preview.png" />
+          <meta property="og:image" content="http://www.super-stipendium.de/img/page-preview.png" />
+          <meta property="og:url" content="https://www.super-stipendium.de/" />
           <meta property="og:type" content="Website" />
-          <meta property="og:title" content="Super-code.de - Programmieren lernen war noch nie digitaler." />
+          <meta property="og:title" content="super-stipendium.de - Erhalte ein Super-Stipendium
+        und lerne Programmieren!" />
           <title>Super-code</title>
           <link rel="icon" href="/favicon.ico" />
 
