9241757e44a147b02614f432337e790feafc2888
DevM423 <m423.dev@gmail.com>
2021-01-29 16:51:33 +0100

    preview picture

--- component/Layout.js
+++ component/Layout.js
@@ -8,11 +8,11 @@ class Layout extends Component {
         <Head>
           <meta
             property="og:image:secure"
-            content="https://super-stipendium.de/img/Webentwickler_Düsseldorf.webp"
+            content="https://super-stipendium.de/img/Webentwickler_Duesseldorf.webp"
           />
           <meta
             property="og:image"
-            content="http://super-stipendium.de/img/Webentwickler_Düsseldorf.webp"
+            content="http://super-stipendium.de/img/Webentwickler_Duesseldorf.webp"
           />
           <meta property="og:url" content="https://super-stipendium.de/" />
           <meta property="og:type" content="Website" />
