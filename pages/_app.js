c1f5a11dc06e3f4a85ee556c274b1bb99a7f59a0
couragesean <seancourage23@gmail.com>
2021-07-28 12:40:18 +0200

    tagmanager

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import 'react-multi-carousel/lib/styles.css';
-import Router from "next/router";
+import Router from 'next/router';
 // import withFBQ from "next-fbq";
-import withAnalytics from "next-analytics";
+import withAnalytics from 'next-analytics';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-MRLL6RX' });
+  }, []);
+  return <Component {...pageProps} />;
 }
 // export default withFBQ("353903998812058", Router)(MyApp);
-export default withAnalytics(Router, { ga: "UA-140157887-1", fbq: "353903998812058" })(MyApp);
+export default withAnalytics(Router, {
+  ga: 'UA-140157887-1',
+  fbq: '353903998812058',
+})(MyApp);
