e916e2c81fa14ef2364064e51e55c9dc38cd1c53
Georg Treitz <georg.treitz@gmail.com>
2021-09-21 11:35:27 +0200

    _error widht removed & ssr

--- pages/_error.js
+++ pages/_error.js
@@ -1,24 +1,20 @@
-import { useWindowSize } from "../component/functions/windowSize";
 import Layout from "../component/Layout";
 import Header404 from "../component/Header404";
 import Footer from "../component/Footer";
 
 export default function Custom404() {
-  if (process.browser) {
-    const width = useWindowSize().width / 12;
-    return (
-      <Layout>
-        <div className="container">
-          <main>
-            <Header404 width={width} />
-            <Footer width={width} />
-          </main>
 
-          <style jsx>{``}</style>
-        </div>
-      </Layout>
-    );
-  } else {
-    return <h1>404 - Page Not Found</h1>;
-  }
+  return (
+    <Layout>
+      <div className="container">
+        <main>
+          <Header404 />
+          <Footer />
+        </main>
+
+        <style jsx>{``}</style>
+      </div>
+    </Layout>
+  );
 }
+
