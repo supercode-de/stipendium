0038b050c72fff498e6d845012fb748e102f9a5a
Finnschy <skylerfearless@yahoo.de>
2022-05-05 17:10:00 +0200

    fehlerhaft importierte component korrigiert in index-ux-ui

--- pages/index-ux-ui.js
+++ pages/index-ux-ui.js
@@ -1,14 +1,14 @@
-import Header_neu from '../component/Header_neu';
 import Layout from '../component/Layout';
-import JetztBewerben_neu from '../component/JetztBewerben_neu';
-import Partner_neu from '../component/Partner_neu';
-import SupercodeUsp2_neu from '../component/SupercodeUsp2_neu';
-import Footer from '../component/Footer';
-import WarumWeb_neu from '../component/WarumWeb_neu';
-import CTA4_neu from '../component/CTA4_neu';
-import SuperStipendium_neu from '../component/SuperStipendium_neu';
+import Header_neu from '../component/Header_neu';
+import JetztBewerbenBob from "../component/JetztBewerben_Bob"
 import HtmlCssWtf_neu from '../component/HtmlCssWtf_neu';
+import SuperStipendium_neu from '../component/SuperStipendium_neu';
+import WarumWeb_neu from '../component/WarumWeb_neu';
 import WarumSC_neu from '../component/WarumSC_neu';
+import SupercodeUsp2_neu from '../component/SupercodeUsp2_neu';
+import Partner_neu from '../component/Partner_neu';
+import CTA4_neu from '../component/CTA4_neu';
+import Footer from '../component/Footer';
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
       <div className='container'>
         <main>
           <Header_neu />
-          <JetztBewerben_neu />
+          <JetztBewerbenBob />
           <HtmlCssWtf_neu />
           <SuperStipendium_neu />
           <WarumWeb_neu />
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import Header_neu from '../component/Header_neu';
 import Layout from '../component/Layout';
-// import JetztBewerben_Emilija from '../component/JetztBewerben_Emilija';
+import Header_neu from '../component/Header_neu';
 import JetztBewerben_Bob from '../component/JetztBewerben_Bob';
-import Partner_neu from '../component/Partner_neu';
-// import SupercodeUsp2_neu from '../component/SupercodeUsp2_neu';
-import Footer from '../component/Footer';
-import WarumWeb_neu from '../component/WarumWeb_neu';
-import CTA4_neu from '../component/CTA4_neu';
-import SuperStipendium_neu from '../component/SuperStipendium_neu';
+// import JetztBewerben_Emilija from '../component/JetztBewerben_Emilija';
 import HtmlCssWtf_neu from '../component/HtmlCssWtf_neu';
+import SuperStipendium_neu from '../component/SuperStipendium_neu';
+import WarumWeb_neu from '../component/WarumWeb_neu';
 import WarumSC_neu from '../component/WarumSC_neu';
 import ShetaniEinsatz from '../component/ShetaniEinsatz';
+// import SupercodeUsp2_neu from '../component/SupercodeUsp2_neu';
+import Partner_neu from '../component/Partner_neu';
+import CTA4_neu from '../component/CTA4_neu';
+import Footer from '../component/Footer';
 
 export default function Home() {
   return (
